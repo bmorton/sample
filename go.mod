5905818260b08a88307d4ea0e5a84f8264f6399c
Brian Morton <bmorton@firehydrant.com>
2022-12-07 11:00:00 -0800

    Add update command

--- go.mod
+++ go.mod
@@ -2,10 +2,25 @@ module github.com/bmorton/sample
 
 go 1.19
 
-require github.com/urfave/cli/v2 v2.23.6
+require (
+	github.com/creativeprojects/go-selfupdate v0.6.1
+	github.com/urfave/cli/v2 v2.23.6
+)
 
 require (
+	code.gitea.io/sdk/gitea v0.15.1 // indirect
+	github.com/Masterminds/semver/v3 v3.2.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-github/v30 v30.1.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/oauth2 v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/creativeprojects/go-selfupdate"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
+	"runtime"
 )
+
+const version = "1.0.1"
 
 // This is the example application from https://cli.urfave.org/v2/getting-started/
 func main() {
@@ -13,12 +18,46 @@ func main() {
 		Name:  "sample",
 		Usage: "make an explosive entrance",
 		Action: func(*cli.Context) error {
-			fmt.Println("boom! I say! v1!")
+			fmt.Printf("boom! I say! v%s!\n", version)
 			return nil
 		},
+		Commands: []*cli.Command{updateCommand},
 	}
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+var updateCommand = &cli.Command{
+	Name:  "update",
+	Usage: "Update the application to the latest version",
+	Action: func(c *cli.Context) error {
+		return update(version)
+	},
+}
+
+func update(version string) error {
+	latest, found, err := selfupdate.DetectLatest("bmorton/sample")
+	if err != nil {
+		return fmt.Errorf("error occurred while detecting version: %w", err)
+	}
+	if !found {
+		return fmt.Errorf("latest version for %s/%s could not be found from github repository", runtime.GOOS, runtime.GOARCH)
+	}
+
+	if latest.LessOrEqual(version) {
+		log.Printf("Current version (%s) is the latest", version)
+		return nil
+	}
+
+	exe, err := os.Executable()
+	if err != nil {
+		return errors.New("could not locate executable path")
+	}
+	if err := selfupdate.UpdateTo(latest.AssetURL, latest.AssetName, exe); err != nil {
+		return fmt.Errorf("error occurred while updating binary: %w", err)
+	}
+	log.Printf("Successfully updated to version %s", latest.Version())
+	return nil
 }
